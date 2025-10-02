e669ba68a8771ff5c0d426e262ce0653b71efa8b
adityapaul1219 <aditya.paul1219@gmail.com>
2025-10-02 14:14:33 -0400

    Update Revenue Code.py

--- Revenue Code.py
+++ Revenue Code.py
@@ -9,10 +9,7 @@ fiscal_year = input("Enter fiscal year: ")
 base_url = "https://api.fiscaldata.treasury.gov/services/api/fiscal_service/v1/accounting/mts/mts_table_1"
 
 #Gets totals
-parameters = {
-    "filter": f"record_date:eq:{fiscal_year}-09-30",
-    "page[size]": 100
-}
+params = {"filter": f"record_date:eq:{fiscal_year}-09-30","page[size]": 100}
 
 response = requests.get(base_url, params=params)
 response.raise_for_status()
@@ -28,14 +25,15 @@ for entry in revenue_data:
     category = entry.get("classification_desc", "N/A")
     year = fiscal_year
     
-    # Gets the 
+    #Gets the amounts/receipts
     amounts = (entry.get("current_month_rcpt_amt") or entry.get("fytd_rcpt_amt") or entry.get("current_month_gross_rcpt_amt") or entry.get("fytd_gross_rcpt_amt") or "0")
     
     try:
         amounts = float(amounts)
     except (ValueError, TypeError):
         amounts = 0.0
-
+    if "FY" in category and category != f"FY {fiscal_year}":
+        continue
     # prints data
     revenue_list.append({
         "Fiscal Year": year,
@@ -43,7 +41,7 @@ for entry in revenue_data:
         "Total Receipts (Millions)": amounts
     })
     
-    print(f"{category} ({year}) - Total Amounts: ${amounts:,.2f} million")
+    print(f"{category} ({year}) - Total Amounts: ${amounts:,.2f}")
     
     sleep(0.1)
 
@@ -53,4 +51,4 @@ output_file = f"us_revenue_{fiscal_year}.csv"
 df_revenue.to_csv(output_file, index=False)
 
 print(f"\nSaved revenue data for {len(df_revenue)} categories to {output_file}")
-df_revenue.head(28
+df_revenue.head(28)
